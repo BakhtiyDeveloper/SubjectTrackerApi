2f4d3ead11ca8ac2bcce71d238163dbda11898f1
BakhtiyDeveloper <bakhtiyer.kurbanov@bk.ru>
2024-09-10 20:33:14 +0500

    Inisialize dataBase

---
+++ SubjectTrackerApi/Brokers/StorageBroker.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using STX.EFxceptions.SqlServer;
+using SubjectTrackerApi.Models;
+
+namespace SubjectTrackerApi.Brokers
+{
+    public interface IStorageBroker
+    {
+
+    }
+
+    public class StorageBroker : EFxceptionsContext, IStorageBroker
+    {
+        private IConfiguration configuration;
+
+        public StorageBroker(IConfiguration konsconfiguration)
+        {
+            this.configuration = konsconfiguration;
+            this.Database.Migrate();
+        }
+
+        public DbSet <Subject> subjects { get; set; }   
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionPath = this.configuration.GetConnectionString("DefaulConnection");
+            optionsBuilder.UseSqlServer(connectionPath); 
+        }
+    }
+}
--- SubjectTrackerApi/Program.cs
+++ SubjectTrackerApi/Program.cs
@@ -1,4 +1,6 @@
 
+using SubjectTrackerApi.Brokers;
+
 namespace SubjectTrackerApi
 {
     public class Program
@@ -8,8 +10,9 @@ namespace SubjectTrackerApi
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
+            builder.Services.AddTransient<IStorageBroker, StorageBroker>();
             builder.Services.AddControllers();
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
